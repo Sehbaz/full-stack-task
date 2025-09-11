9ea0ccf03f8a1642c2963d0c6c8b86200380e4da
Sehbaz Rafik <sehbazm@gmail.com>
2025-09-11 12:54:59 +0100

    feat: add express server and dependencies

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,7 @@
-const http = require("node:http");
+import app from "./app";
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req: any, res: any) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log("api listening to port", port);
 });
