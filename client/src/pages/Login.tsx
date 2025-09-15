0ec638ed3a2daf745e0a98d211ec189fc3d2a225
Sehbaz Rafik <sehbazm@gmail.com>
2025-09-15 10:14:37 +0100

    feat: notification snackbar added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,58 +1,83 @@
 import { Button, Divider, Stack, TextField, Typography } from "@mui/material";
 import AuthLayout from "../components/auth/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
 
-const Login = () => (
-  <AuthLayout
-    title="Log in"
-    subtitle="Login to keep your projects flowing."
-    gradient="radial-gradient(circle at 30% 30%, #a6cdfb, #5a9cfb, #1d5edb)"
-    accentColor="#5a9cfb"
-  >
-    <Stack spacing={2} mb={2}>
-      <TextField
-        label="Your email"
-        aria-label="email"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        label="Password"
-        type="password"
-        aria-label="user-password"
-        variant="outlined"
-        fullWidth
-      />
+const Login = () => {
+  const navigate = useNavigate();
 
-      <Button
-        variant="contained"
-        fullWidth
-        size="large"
-        aria-label="login"
-        sx={{
-          boxShadow: 10,
-          py: 1.3,
-          backgroundColor: "black",
-          color: "white",
-        }}
-      >
-        Log in
-      </Button>
-      <Divider sx={{ my: 2 }}>
-        <Typography align="center" variant="body2" color="text.secondary">
-          or
-        </Typography>
-      </Divider>
-      <Typography align="center" variant="body2" color="text.secondary">
-        Already have an account?{" "}
-        <span
-          style={{ color: "#fb8585", cursor: "pointer" }}
-          onClick={() => (window.location.href = "/register")}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const submitLogin = () => {
+    if (email === "" || password === "") setOpen(true);
+    // navigate("/");
+  };
+
+  return (
+    <AuthLayout
+      title="Log in"
+      subtitle="Login to keep your projects flowing."
+      gradient="radial-gradient(circle at 30% 30%, #a6cdfb, #5a9cfb, #1d5edb)"
+      accentColor="#5a9cfb"
+    >
+      <Stack spacing={2} mb={2}>
+        <TextField
+          label="Your email"
+          aria-label="email"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          label="Password"
+          type="password"
+          aria-label="user-password"
+          variant="outlined"
+          fullWidth
+        />
+
+        <Button
+          variant="contained"
+          fullWidth
+          size="large"
+          aria-label="login"
+          sx={{
+            boxShadow: 10,
+            py: 1.3,
+            backgroundColor: "black",
+            color: "white",
+          }}
+          onClick={submitLogin}
         >
-          Register
-        </span>
-      </Typography>
-    </Stack>
-  </AuthLayout>
-);
+          Log in
+        </Button>
+        <Divider sx={{ my: 2 }}>
+          <Typography align="center" variant="body2" color="text.secondary">
+            or
+          </Typography>
+        </Divider>
+        <Typography align="center" variant="body2" color="text.secondary">
+          Already have an account?{" "}
+          <span
+            style={{ color: "#fb8585", cursor: "pointer" }}
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </span>
+        </Typography>
+      </Stack>
+      <Snackbar
+        open={open}
+        onClose={() => setOpen(false)}
+        autoHideDuration={2000}
+        message="Notification test"
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      />
+    </AuthLayout>
+  );
+};
 
 export default Login;
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,75 +1,100 @@
-import { Button, Divider, Stack, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  Snackbar,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import AuthLayout from "../components/auth/AuthLayout";
+import { useState } from "react";
 
-const Register = () => (
-  <AuthLayout
-    title="Create an account"
-    subtitle="Simple account creation."
-    gradient="radial-gradient(circle at 60% 40%, #fde1c8 0%, #faa974 50%, #fb8585 100%)"
-    accentColor="#fb8585"
-  >
-    <Stack spacing={2} mb={2}>
-      <TextField
-        id="name"
-        label="Your name"
-        aria-label="name"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        id="email"
-        label="Your email"
-        aria-label="email"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        id="user-password"
-        label="Password"
-        type="password"
-        aria-label="user-password"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        id="user-confirm-password"
-        label="Confirm password"
-        type="password"
-        aria-label="user-confirm-password"
-        variant="outlined"
-        fullWidth
-      />
-      <Button
-        id="register"
-        variant="contained"
-        fullWidth
-        size="large"
-        aria-label="register"
-        sx={{
-          boxShadow: 10,
-          py: 1.3,
-          backgroundColor: "black",
-          color: "white",
-        }}
-      >
-        Create account
-      </Button>
-    </Stack>
-    <Divider sx={{ my: 2 }}>
+const Register = () => {
+  const [open, setOpen] = useState(false);
+
+  const submitRegister = () => {
+    setOpen(true);
+    // navigate("/");
+  };
+
+  return (
+    <AuthLayout
+      title="Create an account"
+      subtitle="Simple account creation."
+      gradient="radial-gradient(circle at 60% 40%, #fde1c8 0%, #faa974 50%, #fb8585 100%)"
+      accentColor="#fb8585"
+    >
+      <Stack spacing={2} mb={2}>
+        <TextField
+          id="name"
+          label="Your name"
+          aria-label="name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="email"
+          label="Your email"
+          aria-label="email"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="user-password"
+          label="Password"
+          type="password"
+          aria-label="user-password"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="user-confirm-password"
+          label="Confirm password"
+          type="password"
+          aria-label="user-confirm-password"
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          id="register"
+          variant="contained"
+          fullWidth
+          size="large"
+          aria-label="register"
+          sx={{
+            boxShadow: 10,
+            py: 1.3,
+            backgroundColor: "black",
+            color: "white",
+          }}
+          onClick={submitRegister}
+        >
+          Create account
+        </Button>
+      </Stack>
+      <Divider sx={{ my: 2 }}>
+        <Typography align="center" variant="body2" color="text.secondary">
+          or
+        </Typography>
+      </Divider>
       <Typography align="center" variant="body2" color="text.secondary">
-        or
+        Already have an account?{" "}
+        <span
+          style={{ color: "#fb8585", cursor: "pointer" }}
+          onClick={() => (window.location.href = "/login")}
+        >
+          Log in
+        </span>
       </Typography>
-    </Divider>
-    <Typography align="center" variant="body2" color="text.secondary">
-      Already have an account?{" "}
-      <span
-        style={{ color: "#fb8585", cursor: "pointer" }}
-        onClick={() => (window.location.href = "/login")}
-      >
-        Log in
-      </span>
-    </Typography>
-  </AuthLayout>
-);
+      <Snackbar
+        open={open}
+        onClose={() => setOpen(false)}
+        autoHideDuration={2000}
+        message="Notification test"
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      />
+    </AuthLayout>
+  );
+};
 
 export default Register;
