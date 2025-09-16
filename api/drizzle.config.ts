8bc4f0fc88a487d9e1d95f5c4827b38f4892f660
Sehbaz Rafik <sehbazm@gmail.com>
2025-09-16 11:11:03 +0100

    fix: drizzle config

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const DATABASE_TEST_URL =
+  "postgres://postgres:postgres@localhost:25432/postgres_test";
+
 export default defineConfig({
   out: "./src/db/drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_TEST_URL!,
+    url: DATABASE_TEST_URL,
   },
 });
